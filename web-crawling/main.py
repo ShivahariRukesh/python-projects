e8f9373a9d305cec5316468296e4f4d06523444e
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-01-06 17:55:14 +0545

    Website url manipulation

--- web-crawling/main.py
+++ web-crawling/main.py
@@ -2,12 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 from dotenv import load_dotenv
 import os
+import csv
+from website import  website_list
 
+
+fields = ['no','website_url']
 load_dotenv()
 
 headers = {
     "User-Agent": "KimiNoNawa/1.0"
 }
+
+def enter_websites_url():
+    with open("websites.csv", "w", newline='') as csv_website_file:
+        writer = csv.writer(csv_website_file)
+        writer.writerow(fields)
+        for index,i in enumerate(website_list):
+            writer.writerow([index,i+1])
+
+
 
 website_url = os.getenv('URL')
 website_code = requests.get(website_url, headers=headers).text
@@ -39,4 +52,8 @@ for word in words_list:
 
 for word in words_count_dict:
     print(f"\n The word \"{word}\"'s count is {words_count_dict[word]}")    
+
+
+
+
 
