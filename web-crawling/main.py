5abf6599aeb15c3b32a612f47fad1ded5566bf07
Shivahari Rukesh <478gnujnoej@gmail.com>
2026-01-09 20:34:05 +0545

    Instead of replacing the current values with the small length values, pushing all the matching values in the lists

--- web-crawling/main.py
+++ web-crawling/main.py
@@ -17,7 +17,7 @@ enter_websites_url(website_list)
 read_all_website_url()
 
 
-res = requests.get(website_list[7])
+res = requests.get(website_list[8])
 
 
 def scrap_json():
@@ -37,15 +37,11 @@ def scrap_html():
         text = i.get_text()
         for word in text.split(' '):
             if word in website_keywords_list:
-                # Trying to keep the  values as short as possible  in the dict 
-            #    But it can be optimised by keeping the words less than equals to 4 and make lists of them
-                if(word in results):
-                    if(len(results[word])>len(text)):
-                        results[word]=text 
-                    else:
-                        continue 
-                else:
-                    results[word]=text
+    
+                if not word in results:
+                    results[word] = []
+                # results[word].append(text[:29])
+                results[word] = [*results[word], text[:29]]
                     
 
 scrap_json() if (res.headers.get('Content-Type').split(';')[0] == "application/json") else scrap_html()
